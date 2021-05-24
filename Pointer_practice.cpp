28dd2ec8ebf7e941e01bf4974a7969683f031b11
Jade Cabral <78224175+Jadec15@users.noreply.github.com>
2021-05-24 10:37:09 -0400

    Update Pointer_practice.cpp

--- Pointer_practice.cpp
+++ Pointer_practice.cpp
@@ -1,28 +1,28 @@
 #include <iostream>
-int max(int* p);
+int max(int* p); //declare max function
 
 int main(void) {
 
 	int numbers[6], i, temp = 0, *p; 
 	
 	
-	for (i = 0; i <= 5; i++) {
+	for (i = 0; i <= 5; i++) { //ask user for 6 numbers
 		std::cout << "Input a number: ";
-		std::cin >> numbers[i];
+		std::cin >> numbers[i]; //input user numbers into an array 
 	} 
-	p = numbers;
-	temp = max(p);
+	p = numbers; //p points to numbers array
+	temp = max(p); //temp is the max of the array 
 	
 	std::cout << "This is the max entered number: " << temp;
 }
 
-int max(int *p) {
+int max(int *p) { //take in a pointer
 	int temp = 0;
-	for (int i = 0; i <= 5; i++) {
+	for (int i = 0; i <= 5; i++) { //for each number store highest in temp
 		if (temp <= p[i]) {
 			temp = p[i];
 		}
 	}
-	return temp;
+	return temp; //return highest number
 }
 
